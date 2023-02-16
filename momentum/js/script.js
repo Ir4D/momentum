d4c620775103aaddd44db73cc213225497e6d405
Ir4D <iradedova@gmail.com>
2023-02-16 14:44:19 +0200

    feat: add image slider

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -4,8 +4,9 @@ const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 const dateOptions = {weekday: 'long', month: 'long', day: 'numeric'};
 const greeting = document.querySelector('.greeting');
-const hours = new Date().getHours();
 const userName = document.querySelector('.name');
+const body = document.querySelector('body');
+let randomNum;
 
 // show current time
 function showTime() {
@@ -21,6 +22,7 @@ function showDate() {
 
 // time of the day: morning, afternoon, evening, night
 function getTimeOfDay() {
+  const hours = new Date().getHours();
   let timeOfDay;
   if (0 <= hours/6 && hours/6 < 1) {
     timeOfDay = 'night';
@@ -33,6 +35,7 @@ function getTimeOfDay() {
   };
   greeting.textContent = `Good ${timeOfDay},`;
   setTimeout(getTimeOfDay, 1000);
+  return timeOfDay;
 }
 
 // add user name to the storage
@@ -51,3 +54,58 @@ showDate();
 getTimeOfDay();
 window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
+
+
+// background renew
+
+function getRandomNum() {
+  randomNum = Math.floor(Math.random() * 20 + 1);
+  console.log(randomNum);
+  return randomNum;
+}
+
+getRandomNum()
+
+function setBackground() {
+  const img = new Image();
+  let timeOfDay = getTimeOfDay();
+  // let backgroundNum = randomNum;
+  if (randomNum < 10) {
+    randomNum = '0' + randomNum;
+  };
+  img.src = `https://raw.githubusercontent.com/Ir4D/stage1-tasks/assets/images/${timeOfDay}/${randomNum}.jpg`;
+  img.onload = () => {
+    body.style.backgroundImage = `url('https://raw.githubusercontent.com/Ir4D/stage1-tasks/assets/images/${timeOfDay}/${randomNum}.jpg')`;
+  }
+  return randomNum;
+}
+
+setBackground();
+
+// background slider
+
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+
+function getSlideNext() {
+  if (randomNum == 20) {
+    randomNum = 1;
+  } else {
+    randomNum++;
+  }
+  setBackground();
+  console.log(randomNum);
+}
+
+function getslidePrev() {
+  if (randomNum == 1) {
+    randomNum = 20;
+  } else {
+    randomNum--;
+  }
+  setBackground();
+  console.log(randomNum);
+}
+
+slideNext.addEventListener('click', getSlideNext);
+slidePrev.addEventListener('click', getslidePrev);
