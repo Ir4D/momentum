2c7e3a579087cdbef3169f8a3eb5480c49b6ed3e
Ir4D <iradedova@gmail.com>
2023-02-18 22:53:57 +0200

    feat: add weather widget

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -7,20 +7,31 @@ const greeting = document.querySelector('.greeting');
 const userName = document.querySelector('.name');
 const body = document.querySelector('body');
 let randomNum;
+const slideNext = document.querySelector('.slide-next');
+const slidePrev = document.querySelector('.slide-prev');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const wind = document.querySelector('.wind');
+const humidity = document.querySelector('.humidity')
+const city = document.querySelector('.city');
+const weatherError = document.querySelector('.weather-error');
 
-// show current time
+/* show current time */
 function showTime() {
   time.textContent = new Date().toLocaleTimeString();
   setTimeout(showTime, 1000);
 }
+showTime();
 
-// show date: 'Tuesday, 14 February'
+/* show date: 'Tuesday, 14 February' */
 function showDate() {
   date.textContent = new Date().toLocaleDateString('en-GB', dateOptions);
   setTimeout(showDate, 1000);
 }
+showDate();
 
-// time of the day: morning, afternoon, evening, night
+/* time of the day: morning, afternoon, evening, night */
 function getTimeOfDay() {
   const hours = new Date().getHours();
   let timeOfDay;
@@ -37,39 +48,37 @@ function getTimeOfDay() {
   setTimeout(getTimeOfDay, 1000);
   return timeOfDay;
 }
+getTimeOfDay();
 
-// add user name to the storage
+/* add user name and city to the storage */
 function setLocalStorage() {
   localStorage.setItem('userName', userName.value);
+  localStorage.setItem('city', city.value);
 }
+window.addEventListener('beforeunload', setLocalStorage);
 
 function getLocalStorage() {
   if (localStorage.getItem('userName')) {
     userName.value = localStorage.getItem('userName');
   }
+  if (localStorage.getItem('city')) {
+    city.value = localStorage.getItem('city');
+  } else {
+    city.value = 'Minsk';
+  }
 }
-
-showTime();
-showDate();
-getTimeOfDay();
-window.addEventListener('beforeunload', setLocalStorage);
 window.addEventListener('load', getLocalStorage);
 
-
-// background renew
-
+/* background renew */
 function getRandomNum() {
   randomNum = Math.floor(Math.random() * 20 + 1);
-  console.log(randomNum);
   return randomNum;
 }
-
 getRandomNum()
 
 function setBackground() {
   const img = new Image();
   let timeOfDay = getTimeOfDay();
-  // let backgroundNum = randomNum;
   if (randomNum < 10) {
     randomNum = '0' + randomNum;
   };
@@ -79,14 +88,9 @@ function setBackground() {
   }
   return randomNum;
 }
-
 setBackground();
 
-// background slider
-
-const slideNext = document.querySelector('.slide-next');
-const slidePrev = document.querySelector('.slide-prev');
-
+/* background slider */
 function getSlideNext() {
   if (randomNum == 20) {
     randomNum = 1;
@@ -94,8 +98,8 @@ function getSlideNext() {
     randomNum++;
   }
   setBackground();
-  console.log(randomNum);
 }
+slideNext.addEventListener('click', getSlideNext);
 
 function getslidePrev() {
   if (randomNum == 1) {
@@ -104,8 +108,37 @@ function getslidePrev() {
     randomNum--;
   }
   setBackground();
-  console.log(randomNum);
 }
-
-slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getslidePrev);
+
+/* weather widget */
+async function getWeather() {
+  if (city.value === '') {
+    city.value = 'Minsk';
+  }
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=0fe1a130729e5592daaf209c9d35966d&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  if (data.cod == '404' || !city.value === ' ') {
+    console.log('Not found');
+    weatherError.textContent = `Error! City not found for '${city.value}'!`;
+    weatherIcon.style.display = 'none';
+    temperature.textContent = '';
+    weatherDescription.textContent = '';
+    wind.textContent = '';
+    humidity.textContent = '';
+  } else {
+    weatherError.textContent = '';
+    weatherIcon.style.display = 'inline';
+    weatherIcon.className = 'weather-icon owf';
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.floor(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${Math.floor(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${Math.floor(data.main.humidity)}%`;
+  }
+}
+city.addEventListener('change', getWeather);
+window.addEventListener('load', () => {
+  getWeather()
+});
